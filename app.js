adae55f31f9d1f8f4b3f5c50f816962720ad0caa
Ryan Dalton <daltonr121@gmail.com>
2024-04-24 14:01:37 -0400

    Refactored timestamp

--- app.js
+++ app.js
@@ -1,86 +1,108 @@
-require('dotenv').config(); //This will be used to store private keys
-const path = require('path');
-const fs = require('fs');
-const deployCommands = require('./deploy/deployCommands');
-const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
-const getMeme = require('./commands/getMeme/getMeme');
+require("dotenv").config(); //This will be used to store private keys
+const path = require("path");
+const fs = require("fs");
+const deployCommands = require("./deploy/deployCommands");
+const {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  REST,
+  Routes,
+} = require("discord.js");
+const getMeme = require("./commands/getMeme/getMeme");
 
 const BOT_TOKEN = process.env.CLIENT_TOKEN;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
-
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
 }
 
 //Register our commands
 deployCommands();
 
-
-
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * 9) + 1;
-}
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
 
+  const randomNumber = (message.createdTimestamp % 9) + 1;
 
-client.on('messageCreate', async (message) => {
-	if (message.author.globalName === 'stiffsock6699') {
-		console.log(message.author.id);
-		console.log(message.content);
-	}
+  if (randomNumber === 5) {
+    if (
+      message.author.id === "475256575926206474" &&
+      message.channelId === "1032494913885065276" &&
+      message.createdTimestamp.endsWith(getRandomNumber())
+    ) {
+      message.reply("Shutup you stupid skank.");
+    }
 
-	if (message.author.id === '475256575926206474' && message.channelId === '1032494913885065276' && message.createdTimestamp.endsWith(getRandomNumber())) {
-		message.reply('Shutup you stupid skank.')
-	}
-
-	if (message.author.id === '495321423045328906' && message.channelId === '1032494913885065276' && message.createdTimestamp.endsWith(getRandomNumber())) {
-		message.reply('Thank you for your message.')
-	}
-
-    if (message.author.bot) return;
+    if (
+      message.author.id === "495321423045328906" &&
+      message.channelId === "1032494913885065276" &&
+      message.createdTimestamp.endsWith(getRandomNumber())
+    ) {
+      message.reply("Thank you for your message.");
+    }
+  }
 });
 
 client.login(BOT_TOKEN);
