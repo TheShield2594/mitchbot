baa405c9a7328ee046c16cacd0bbff929e398c43
Brandon T. Shields <brandots@everence.com>
2024-04-24 16:14:38 -0400

    Removal of unused items

--- commands/utility/choose.js
+++ commands/utility/choose.js
@@ -3,16 +3,16 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("choose")
-        .setDescription("I can't decide... Decide for me.")
+        .setDescription("Decide for me.")
         .addStringOption((option) =>
             option
                 .setName("choices")
                 .setRequired(true)
-                .setDescription("What Choices? Seperate choices by using or")
+                .setDescription("Seperate with OR between choices")
         ),
     async execute(interaction) {
         const choiceInput = interaction.options.getString("choices");
-        splitOptions = choiceInput.split(" or ");
+        splitOptions = choiceInput.split(" OR ");
         randomAnswer =
             splitOptions[Math.floor(Math.random() * splitOptions.length)];
         await interaction.reply(
