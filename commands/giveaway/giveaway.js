9e063945f6acc87ecc18d7c5f7757c1958ac587c
Brandon Shields <bshields2594@gmail.com>
2024-05-08 12:02:06 -0400

    giveaway change v3

--- commands/giveaway/giveaway.js
+++ commands/giveaway/giveaway.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { SlashCommandBuilder } = require("discord.js");
 
 client.on('ready', () => {
   console.log('I am ready!');
