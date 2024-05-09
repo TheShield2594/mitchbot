897e97bdace6ee98310cc962e497637b58b25fd3
Brandon Shields <bshields2594@gmail.com>
2024-05-09 15:26:13 -0400

    Fix Praise?

--- commands/fun/praise.js
+++ commands/fun/praise.js
@@ -8,15 +8,36 @@ module.exports = {
         .addUserOption((option) =>
             option.setName("user").setDescription("Let's be nice to people.")),
     async execute(interaction) {
-        const complimentURL = await request("https://complimentr.com/api/");
-        const { compliments } = await complimentURL.body.json();
-
-        await interaction.deferReply();
-        const message = await interaction.editReply(
-            `${
-                interaction.options.getUser("user") ?? interaction.user
-            } ${compliments[Math.floor(Math.random() * compliments.length)]}`
-        );
-        message.react("🙏");
+        const compliments = [
+            "You have the biggest of dicks",
+            "You make me cream",
+            "You're a fantastic person!",
+            "You're a true gem!",
+            "You're a true friend!",
+            "You're a true inspiration!",
+            "You're a true hero!",
+            "You're a true leader!",
+            "You're a true visionary!",
+            "You're a hard worker!",
+            "You're well hung",
+            "You're huge!",
+            "You're amazing!",
+            "You're a true patriot!",
+            "You have a nice package!",
+            "You're a stud",
+            "You're a true innovator!",
+            "You're great!",
+            "You're a true philosopher!",
+            "You make me think dirty things!",
+            "You're a true problem solver!",
+            "You're awesome",
+            "You're a true chick magnet",
+            "You're naughty",
+            "You're a hard worker!",
+            "You're a stud muffin!",
+            "You're a girthy boy",
+            "You have lovely eyes",
+            "You're a big boy",
+ , ]
     }
 };
