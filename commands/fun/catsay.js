6f0688f9cd9ff5959dc8d14a2926b362ade4a719
Brandon Shields <bshields2594@gmail.com>
2024-05-22 11:54:56 -0400

    cat speak to gif

--- commands/fun/catsay.js
+++ commands/fun/catsay.js
@@ -1,31 +1,18 @@
-const { request } = require("undici");
-const { SlashCommandBuilder } = require("discord.js");
+const axios = require('axios');
+const { SlashCommandBuilder } = require('discord.js');
+
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("meow")
-        .setDescription("meow meow meow meow meow meow meow")
-        .addUserOption((option) =>
-            option.setName("user").setDescription("Pspspspspspspspspspsps")
-        ),
-    async execute(interaction) {
-       
-        try {
-            const response = await fetch('https://cataas.com/cat/gif');
-            const data = await response.json();
-            const { insult } = data;
-
-            await interaction.deferReply();
-            const message = await interaction.editReply(
-                `${
-                    interaction.options.getUser("user") ?? interaction.user
-                } ${insult}`
-            );
-            message.react("🔥");
-        } catch (err) {
-            console.log('error: ', err);
-            await interaction.reply("Failed to fetch an image. Please try again later bitch.");
-            return;
+	data: new SlashCommandBuilder()
+		.setName('cat gif')
+		.setDescription('Sends a random cat gif'),
+	async execute(interaction) {
+        const res = await axios.get('https://cataas.com/cat/gif');
+        if (res.data.memes[0].url){
+            interaction.reply(res.data.memes[0].url);
         }
-    },
+        else{
+            interaction.reply("No gif found :(");
+        }
+	},
 };
