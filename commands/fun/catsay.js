f23e036b55fe421159ba32cacd1a5aa74134f32e
Brandon Shields <bshields2594@gmail.com>
2024-05-22 11:59:24 -0400

    fix the code for bot

--- commands/fun/catsay.js
+++ commands/fun/catsay.js
@@ -1,18 +1,23 @@
 const axios = require('axios');
 const { SlashCommandBuilder } = require('discord.js');
 
-
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('cat gif')
-		.setDescription('Sends a random cat gif'),
-	async execute(interaction) {
-        const res = await axios.get('https://cataas.com/cat/gif');
-        if (res.data.memes[0].url){
-            interaction.reply(res.data.memes[0].url);
+    data: new SlashCommandBuilder()
+        .setName('cat-gif')
+        .setDescription('Sends a random cat gif'),
+    async execute(interaction) {
+        try {
+            const res = await axios.get('https://cataas.com/cat/gif');
+            if (res.data.memes && res.data.memes.length > 0) {
+                const randomIndex = Math.floor(Math.random() * res.data.memes.length);
+                const randomGif = res.data.memes[randomIndex].url;
+                await interaction.reply(randomGif);
+            } else {
+                await interaction.reply("No gif found :(");
+            }
+        } catch (err) {
+            console.error('Error fetching gif:', err);
+            await interaction.reply("Failed to fetch a gif. Please try again later.");
         }
-        else{
-            interaction.reply("No gif found :(");
-        }
-	},
+    },
 };
