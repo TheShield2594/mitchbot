fd61cb3c15ad5f5fae58b62f22080b949315a077
Brandon Shields <bshields2594@gmail.com>
2024-05-22 10:30:06 -0400

    test catsay

---
+++ commands/fun/catsay.js
@@ -0,0 +1,24 @@
+const { request } = require("undici");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("meow")
+        .setDescription("Meow Meow Meow Meow!")
+        .addStringOption((option) =>
+            option.setName("catsay").setDescription("The text for catsay")
+        ),
+    async execute(interaction) {
+        const catsayText =
+            interaction.options.getString("catsay") ?? "Meow Meow Meow Meow!";
+        splitMessage = catsayText.split(" ");
+        joinedMessage = splitMessage.join("+");
+
+        const catURL = await request(
+            `https://cataas.com/cat/cute/says/${msg}`
+        );
+        const { cat } = await mooURL.body.json();
+        await interaction.deferReply();
+        await interaction.editReply("```" + cat + "```");
+    },
+};
