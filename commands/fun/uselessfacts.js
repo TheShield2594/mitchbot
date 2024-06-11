b6ab3f607a2d004917a497611bcfbd80601e8936
Ryan Dalton <daltonr121@gmail.com>
2024-06-11 15:55:32 -0400

    Removed testing console logs

--- commands/fun/uselessfacts.js
+++ commands/fun/uselessfacts.js
@@ -18,11 +18,10 @@ module.exports = {
                     Accept: "application/json"
                 }
             });
-            console.log(response);
 
             const data = await response.json(); //correctly parse the JSON from the response
-            console.log(data);
             const {text: fact } = data; //extract the fact from the parsed data
+            
             if (fact) {
                 await interaction.reply(`${fact}`);
             } else {
