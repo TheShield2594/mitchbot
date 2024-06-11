bc42683d909f0269d74fd5825fec381202cba151
Ryan Dalton <daltonr121@gmail.com>
2024-06-10 23:40:34 -0400

    /choose fix
    I believe this commit will fix it.  I don't see where we were setting the string options we were trying to pull in the execute method.  Commiting and pushing to test since I'm having trouble testing locally

--- commands/utility/choose.js
+++ commands/utility/choose.js
@@ -1,14 +1,23 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("choose")
-        .setDescription("Decide for me."),
-        async execute(interaction) {
-            const choiceInput = interaction.options.getString("choices");
-            const splitOptions = choiceInput.split(" OR ");
-            const randomAnswer = splitOptions[Math.floor(Math.random() * splitOptions.length)];
-            await interaction.reply(`\`\`${interaction.user.username} asks: ${choiceInput}\`\``);
-            await interaction.channel.send(randomAnswer);
-        }
-    }
+  data: new SlashCommandBuilder()
+    .setName("choose")
+    .setDescription("Decide for me.")
+    .addStringOption((option) =>
+      option
+        .setName("choices")
+        .setDescription("Must be formatted as '(option 1) OR (option 2)'")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const choiceInput = interaction.options.getString("choices");
+    const splitOptions = choiceInput.split(" OR ");
+    const randomAnswer =
+      splitOptions[Math.floor(Math.random() * splitOptions.length)];
+    await interaction.reply(
+      `\`\`${interaction.user.username} asks: ${choiceInput}\`\``
+    );
+    await interaction.channel.send(randomAnswer);
+  },
+};
