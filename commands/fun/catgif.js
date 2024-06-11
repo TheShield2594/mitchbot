5a91cd89af67933eb84aa05b26829cbecf829aa7
Brandon Shields <bshields2594@gmail.com>
2024-06-11 16:28:49 -0400

    URL test

--- commands/fun/catgif.js
+++ commands/fun/catgif.js
@@ -1,34 +1,18 @@
-const { request } = require("undici");
-const { SlashCommandBuilder } = require("discord.js");
+const axios = require('axios');
+const { SlashCommandBuilder } = require('discord.js');
 
-async function fetchData(url, options) {
-    const { body } = await request(url, options);
-    return body.json();
-}
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("cat")
-        .setDescription("Here is a cat!"),
-
-    async execute(interaction) {
-        try {
-            const response = await fetchData("https://cataas.com/cat?json=true", {
-                headers: {
-                    Accept: "application/json"
-                }
-            });
-
-            const { url: catUrl } = response; //extract the URL from the parsed data
-            
-            if (catUrl) {
-                await interaction.reply({ files: [`https://cataas.com${catUrl}`] });
-            } else {
-                await interaction.reply("No cat found :(");
-            }
-        } catch (error) {
-            console.error(error);
-            await interaction.reply("An error occurred while fetching the cat. Please try again later.");
+	data: new SlashCommandBuilder()
+		.setName('cat')
+		.setDescription('Sends a random cat'),
+	async execute(interaction) {
+        const res = await axios.get('https://cataas.com/cat?position=center');
+        if (res.data.memes[0].url){
+            interaction.reply(res.data.cat[0].url);
         }
-    }
+        else{
+            interaction.reply("No cat found :(");
+        }
+	},
 };
