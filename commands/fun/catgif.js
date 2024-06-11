fd65e9d6dd0786105b9594605d971b09e03aadbe
Brandon Shields <bshields2594@gmail.com>
2024-06-11 16:23:28 -0400

    pushing new url

--- commands/fun/catgif.js
+++ commands/fun/catgif.js
@@ -2,8 +2,8 @@ const { request } = require("undici");
 const { SlashCommandBuilder } = require("discord.js");
 
 async function fetchData(url, options) {
-    const response = await fetch(url, options);
-    return response;
+    const { body } = await request(url, options);
+    return body.json();
 }
 
 module.exports = {
@@ -13,23 +13,22 @@ module.exports = {
 
     async execute(interaction) {
         try {
-            const response = await fetchData("https://cataas.com/cat", {
+            const response = await fetchData("https://cataas.com/cat?json=true", {
                 headers: {
                     Accept: "application/json"
                 }
             });
 
-            const data = await response.json(); //correctly parse the JSON from the response
-            const {text: fact } = data; //extract the fact from the parsed data
+            const { url: catUrl } = response; //extract the URL from the parsed data
             
-            if (fact) {
-                await interaction.reply(`${fact}`);
+            if (catUrl) {
+                await interaction.reply({ files: [`https://cataas.com${catUrl}`] });
             } else {
                 await interaction.reply("No cat found :(");
             }
         } catch (error) {
             console.error(error);
-            await interaction.reply("An error occurred while fetching the fact. Please try again later.");
+            await interaction.reply("An error occurred while fetching the cat. Please try again later.");
         }
     }
 };
