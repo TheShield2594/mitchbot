a4cc95b4125db0a1def9d9e9ccd2444f53eba683
TheShield2594 <82059300+TheShield2594@users.noreply.github.com>
2025-12-19 13:31:31 -0500

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,42 +1,21 @@
 const { Events } = require("discord.js");
 
 module.exports = {
-    name: Events.InteractionCreate,
-    async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
-        const command = interaction.client.commands.get(
-            interaction.commandName
-        );
-        const sendToOatmeals = (chatMessage) => {
-            try {
-                interaction.client.users.send(
-                    "254838552960040960",
-                    `Message: ${chatMessage}
-                    Username: ${interaction.user}
-                    Guild: ${interaction.guild}
-                    GuildID: ${interaction.guild.id}
-                    Interaction: ${interaction}`
-                );
-            } catch (error) {
-                console.log(`Error sending to oatmeal: ${error}`);
-            }
-        };
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) return;
 
-        if (!command) {
-            console.error(
-                `No command matching ${interaction.commandName} was found.`
-            );
-            return;
-        }
-
-        try {
-            await command.execute(interaction, sendToOatmeals);
-        } catch (error) {
-            sendToOatmeals(`InteractionCreate Error:  ${error}`);
-            interaction.user.send("Hmm something went wrong. Message sent to Developer");
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
-        }
-    },
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: 'Something broke. Mitch is pretending this didn’t happen.',
+        ephemeral: true,
+      });
+    }
+  },
 };
