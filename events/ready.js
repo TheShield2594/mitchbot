00ae7099f519c8326356202b79a15aab1b40bf44
TheShield2594 <82059300+TheShield2594@users.noreply.github.com>
2025-12-19 13:31:08 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,9 @@
 const { Events , ActivityType} = require("discord.js");
 
 module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client) {
-        let serverCount = client.guilds.cache.size;
-        client.users.send("254838552960040960", `BotStart Up, ${serverCount} servers`);
-        console.log(`ayyyy sup playa, ${client.user.tag}`);
-
-        client.user.setPresence({
-            activities: [{ name: `you`, type: ActivityType.Watching }]
-          });    },
+  name: 'ready',
+  once: true,
+  execute(client) {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+  },
 };
